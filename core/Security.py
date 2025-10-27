5a2e46753eb5c28ac6168a489e12407a5538da05
mateushlsilva <mateushls01@gmail.com>
2025-10-27 09:29:40 -0300

    feat: Implementa geração de 2FA (TOTP) e QR Code

--- core/Security.py
+++ core/Security.py
@@ -1,7 +1,24 @@
+import pyotp
+import qrcode
+import io
+
 class Security:
 
-    def generate_2fa():
-        pass
+    def generate_2fa(self, username, enterprisename):
+        secret = pyotp.random_base32()
+        otp_uri = pyotp.totp.TOTP(secret).provisioning_uri(
+            name=username,
+            issuer_name=enterprisename
+        )
 
-    def generate_qrcode():
-        pass
+        return otp_uri
+
+    def generate_qrcode(self, username, enterprisename):
+        otp_uri = self.generate_2fa(username, enterprisename)
+
+        qr = qrcode.make(otp_uri)
+        buf = io.BytesIO()
+        qr.save(buf, format='PNG')
+        buf.seek(0)
+
+        return {"content": buf.getvalue(), "media_type": "image/png"}
