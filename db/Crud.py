7bde5cbec19c043e8dca89e82293989bec312b76
mateushlsilva <mateushls01@gmail.com>
2025-10-27 14:07:25 -0300

    feat: Adiciona Crud

---
+++ db/Crud.py
@@ -0,0 +1,24 @@
+from models.user_model import UserModel
+
+class Crud:
+    def __init__(self, db):
+        self.db = db
+
+    async def post(self, user: UserModel):
+        await self.db.users.insert_one(user.to_dict())
+
+    async def get(self, username: str):
+        return await self.db.users.find_one({"username": username})
+
+    async def delete(self, username: str):
+        return await self.db.users.delete_one({"username": username})
+    
+    async def put(self, username, user: UserModel):
+        result = await self.db.users.update_one(
+            {"username": username}, 
+            {"$set": user}           
+        )
+        return {
+            "matched_count": result.matched_count,
+            "modified_count": result.modified_count
+        } 
