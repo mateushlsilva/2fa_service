4b1a7affc22a1fb24b502aaddd4286e371126fbf
mateushlsilva <mateushls01@gmail.com>
2025-10-28 14:47:23 -0300

    feat(api): Implementa rota e lógica de serviço para verificar recovery codes

--- api/v1/endpoints/user_2fa.py
+++ api/v1/endpoints/user_2fa.py
@@ -3,14 +3,14 @@ from fastapi.responses import JSONResponse
 from core.deps import get_db
 from services.User_service import UserService
 import base64
-from schemas.user_schema import UserCreate
+from schemas.user_schema import UserCreate, RecoveryRequest
 
 router = APIRouter(prefix="/2fa", tags=["2FA"])
 
 @router.post("/setup")
-async def setup_2fa(username: UserCreate = Body(...), db=Depends(get_db)):
+async def setup_2fa(user: UserCreate = Body(...), db=Depends(get_db)):
     service = UserService(db)
-    data = await service.register_user(username.username)
+    data = await service.register_user(user.identifier)
     # response.headers["Content-Type"] = data["media_type"]
     # return Response(content=data["qrcode"], media_type=data["media_type"])
     qrcode_base64 = base64.b64encode(data["qrcode"]).decode("utf-8")
@@ -23,6 +23,12 @@ async def setup_2fa(username: UserCreate = Body(...), db=Depends(get_db)):
 
 
 @router.get("/verify")
-async def verify_2fa(username: str = Query(...), code: str = Query(...), db=Depends(get_db)):
+async def verify_2fa(identifier: str = Query(...), code: str = Query(...), db=Depends(get_db)):
     service = UserService(db)
-    return await service.verify_2fa(username, code)
+    return await service.verify_2fa(identifier, code)
+
+
+@router.patch("/recovery/generate")
+async def regenerate_recovery(req: RecoveryRequest, db=Depends(get_db)):
+    service = UserService(db)
+    return await service.verify_recovery(req.identifier, req.code)
--- services/User_service.py
+++ services/User_service.py
@@ -9,19 +9,19 @@ class UserService:
         self.crud = Crud(db)
         self.security = Security()
 
-    async def register_user(self, username: str):
-        existing = await self.crud.get(username)
+    async def register_user(self, identifier: str):
+        existing = await self.crud.get(identifier)
         if existing:
             raise HTTPException(status_code=400, detail="Usuário já existe")
         # Gera QR e secret
-        data = self.security.generate_qrcode(username)
+        data = self.security.generate_qrcode(identifier)
         recovery = self.security.generate_recovery_codes()
-        user = UserModel(username=username, secret=data["secret"], hashed_codes=recovery['hashed_codes'])
+        user = UserModel(identifier=identifier, secret=data["secret"], hashed_codes=recovery['hashed_codes'])
         await self.crud.post(user)
         return {"qrcode": data["qrcode"], "media_type": data["media_type"], "recovery": recovery['codes']}
 
-    async def verify_2fa(self, username: str, code: str):
-        user = await self.crud.get(username)
+    async def verify_2fa(self, identifier: str, code: str):
+        user = await self.crud.get(identifier)
         if not user:
             return {"status": "not_found"}
 
@@ -30,3 +30,14 @@ class UserService:
         if totp.verify(code):
             return {"status": "success"}
         return {"status": "failed"}
+    
+    async def verify_recovery(self, identifier: str, code_input: str):
+        user = await self.crud.get(identifier)
+        if not user:
+            raise HTTPException(status_code=404, detail="Usuário inixistente")
+        verify = self.security.verify_recovery_code(user['hashed_codes'], code_input)
+        if verify == None:
+            raise HTTPException(status_code=404, detail="Código inixistente")
+        user['hashed_codes'].remove(verify)
+        await self.crud.put(identifier, user)
+        return {"status": "success"}
